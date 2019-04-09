da6da2dbff930c0c193b93c5d186eb201a58b6c6
gmclp <gchampion@liveperson.com>
2019-04-09 12:11:13 -0400

    Enhance missing fields action to include when a single field is missing

--- src/Typescript/Fields.ts
+++ src/Typescript/Fields.ts
@@ -1,6 +1,9 @@
 import vscode, { CodeAction, Selection, Range, Uri, WorkspaceEdit, CodeActionKind, Diagnostic, TextDocument } from 'vscode';
 import { cpus } from 'os';
 
+const followingPropertiesText = 'is missing the following properties from type';
+const missingInTypeText = 'is missing in type';
+
 export class Fields implements vscode.CodeActionProvider {
 
   public provideCodeActions(
@@ -16,13 +19,11 @@ export class Fields implements vscode.CodeActionProvider {
   private doSomething(uri: Uri, diagnostics: Diagnostic[], document: TextDocument): CodeAction[] {
     return diagnostics
       .filter((diagnostic) => {
-        return diagnostic.message.includes('is missing the following properties from type');
+        return diagnostic.message.includes(followingPropertiesText)
+          || diagnostic.message.includes(missingInTypeText);
       })
       .map((diagnostic) => {
-        const prefixPosition = diagnostic.message.indexOf('is missing the following properties from type');
-        const colonPosition = diagnostic.message.indexOf(':', prefixPosition);
-        const fieldList = diagnostic.message.substr(colonPosition + 1);
-        const missingFields = fieldList.split(',').map(field => field.trim());
+        const missingFields = getMissingFields(diagnostic);
 
         const nextLineNumber = diagnostic.range.start.line + 1;
         const nextLine = document.lineAt(nextLineNumber);
@@ -35,7 +36,6 @@ export class Fields implements vscode.CodeActionProvider {
         const underneathDeclaration = new vscode.Position(nextLineNumber, 0);
         const workspaceEdit = new WorkspaceEdit();
         workspaceEdit.insert(uri, underneathDeclaration, `${linesToAdd}\n`);
-
 
         const codeAction: CodeAction = {
           title: 'Add missing fields',
@@ -57,4 +57,29 @@ function getIndentation(line: string) {
 function padText(text: string, pad: number) {
   const padding = new Array(pad).join(' ');
   return `${padding}${text}`;
+}
+
+function getMissingFields(diagnostic: Diagnostic): string[] {
+  const message = diagnostic.message;
+
+  console.log(`${message}`);
+
+  if (message.includes(followingPropertiesText)) {
+    const prefixPosition = message.indexOf(followingPropertiesText);
+    const colonPosition = message.indexOf(':', prefixPosition);
+    const fieldList = message.substr(colonPosition + 1);
+    const missingFields = fieldList.split(',').map(field => field.trim());
+    return missingFields;
+  }
+
+  if (message.includes(missingInTypeText)) {
+    const propertyPrefix = 'Property \'';
+    const prefixPosition = message.indexOf(propertyPrefix);
+    const fieldStart = prefixPosition + propertyPrefix.length;
+    const closeQuote = message.indexOf('\'', fieldStart);
+    const field = message.substring(fieldStart, closeQuote).trim();
+    return [field];
+  }
+
+  return [];
 }
